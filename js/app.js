41b5feaa87b3ce00718ca244f609a64b96204258
Modsky1 <maude.jake@gmail.com>
2014-02-28 14:25:19 -0500

    first commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,18 @@
-alert('Hey find me to know where to put the javascript');
+var random_number = Math.floor(Math.random() * 5) + 1
+
+name = prompt("What is your name?")
+
+var switcher = true
+
+while (switcher) {
+  var user_guess = prompt(name + ", please guess a number from 1 to 5.")
+
+  if (user_guess == random_number) {
+    alert("You've won!!!");
+    switcher = false;
+  }
+
+  else {
+    alert("PLEASE TRY AGAIN");
+  }
+}
